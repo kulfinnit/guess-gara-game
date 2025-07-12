8f983eb5029eb58bbca48eb2aa8d848f444c7d33
kulfinnit <ksapkota971@gmail.com>
2025-07-12 21:25:55 +0545

    game start

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "guess-gara-v1";
+const urlsToCache = [
+  "./",
+  "./guess_gara_bhai.html",
+  "./favicon.ico",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap",
+  "https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg",
+  "https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg",
+  "https://actions.google.com/sounds/v1/cartoon/cartoon_boing.ogg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => {
+        self.skipWaiting();
+      }),
+  );
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request)),
+  );
+});
